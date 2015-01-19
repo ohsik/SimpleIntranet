11b50e3d485ad5d50b68fabdc48a7366eeb3dd34
User <Wildctas10>
2015-01-19 17:00:15 -0600

    work in progress
    Packery, Remodal added. style improvement

--- js/si.js
+++ js/si.js
@@ -1,48 +1,70 @@
 $( document ).ready(function() {
     
-    // Draggable for divs
+     // *Packery
+    //----------------------------------------------------------------------------------------------------
+    var $container = $('.packery').packery();
+    
+    // *Enable draggable divs
+    //----------------------------------------------------------------------------------------------------
     $(function() {
         var i = 0;
         var totalnum = 5;
         
         for( i; i < totalnum; i++){
-            $( '#draggable' + i ).draggable({
-                containment: "window"
-            });
-            
-            //Edit button
-            var selecteddiv = "#draggable" + i + " .add-btn > i";
-            var showbtns = "#draggable" + i + " .list-div i";
-            var editbtns = "#draggable" + i + " .edit-btn";
-            alert(selecteddiv);
-            
-            // **Solve this issue
-            $(selecteddiv).click(function (){  
-                $(showbtns).fadeToggle();
-                $(editbtns).fadeToggle();
-            }); 
+            // get item elements, jQuery-ify them
+            var $itemElems = $container.find('#draggable' + i);
+            // make item elements draggable
+            $itemElems.draggable();
+            // bind Draggable events to Packery
+            $container.packery( 'bindUIDraggableEvents', $itemElems );
         }
+    });
+
+    // *Edit button on each divs (When edit icon clicked, show all edit buttons)
+    //----------------------------------------------------------------------------------------------------
+    var selecteddiv = '[id^="draggable"] .add-btn > i';
+    //console.log(selecteddiv);
+    
+    $(selecteddiv).click(function (){ 
+        var parentofthis = $(this).closest('[id^="draggable"]').attr('id');
+        //console.log(parentofthis);
+        $('#' + parentofthis + " .list-div i").fadeToggle();
+        $('#' + parentofthis + " .edit-btn").fadeToggle();
         
-            
-            
-            
-    
+        $("#debug").html(parentofthis);
     });
     
-         
-    
-    
-    //Lightbox Initialize
-    $('.add-list').magnificPopup({
-      items: {
-          src: '<div class="white-popup">Dynamically created popup</div>',
-          type: 'inline'
-      }
+    $('.remove-div').click(function (){
+        var cltxt = $(this).parent().text();
+        $(".clickedtext").html(cltxt);
     });
     
-    //Dialog for removing div
-    $('.remove-div').click(function() {
-        $( "#remove-msg" ).dialog();
+
+    // *Remodal enable by adding data attribute
+    //----------------------------------------------------------------------------------------------------
+    $('.add-list').attr( "data-remodal-target", "modal");
+    $('.remove-div').attr( "data-remodal-target", "rmmodal");
+
+
+    // *Form Select
+    //----------------------------------------------------------------------------------------------------
+    $("#type").change(function(){
+        var selectedform = $('#type').val();
+        //console.log(selectedform);
+        if ( selectedform == "link" ){
+            $(".con-form").hide(); 
+            $("#link").fadeIn();   
+        } else if ( selectedform == "download" ){
+            $(".con-form").hide(); 
+            $("#file").fadeIn(); 
+        } else {
+            $(".con-form").hide(); 
+            $("#content").fadeIn();
+        }
     });
+    
+    
+    
+    
     
 });
